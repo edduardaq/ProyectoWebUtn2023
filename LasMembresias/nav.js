cea4c18330432d127b14e73eff6838c485cf019b
unknown <williamcubero800@gmail.com>
2023-03-23 12:44:00 -0600

    Aporte will

---
+++ LasMembresias/nav.js
@@ -0,0 +1,14 @@
+/*No tocar*/
+
+/* Primer JS creacion de la pagina principar con el menu Nav, Header  */
+
+/*Logica a la hora de abrir la pestana*/
+function openNav(){
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+
+/*Logica a la hora de cerrar la pestana*/
+function closeNav(){
+    document.getElementById("mobile-menu").style.width = "0%";
+}
+
